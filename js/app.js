f8a547676a1400d8961bb51ab69f23648659d209
swaption2009 <swaption2009@gmail.com>
2015-06-26 20:19:29 -0700

    move enemies by incremental value of 1

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += 1;
 }
 
 // Draw the enemy on the screen, required method for game
