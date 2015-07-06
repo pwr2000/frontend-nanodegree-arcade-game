4581c4e270f499afc83fa2441260cfbf9d1fd481
swaption2009 <swaption2009@gmail.com>
2015-07-05 21:48:03 -0700

    randomize enemy speed and multiply with dt parameter

--- js/app.js
+++ js/app.js
@@ -14,7 +14,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += 0.7;
+    this.x += Math.floor((Math.random() * 1) + 0.1) * dt * 50;
+    // console.log(this.x);
 };
 
 // Draw the enemy on the screen, required method for game
